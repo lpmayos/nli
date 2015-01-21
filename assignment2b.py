6f3ba1a6461249f6e6a66673965c995b48a77713
Laura Pérez Mayos <lpmayos@gmail.com>
2015-01-21 12:40:45 +0100

    parse_name

--- assignment2b.py
+++ assignment2b.py
@@ -25,6 +25,7 @@ References:
 """
 
 import sys
+import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -38,6 +39,8 @@ class Graph():
         """
         self.graph = None
         self.user_name = None
+        self.current_node = 'ask_name'
+        self.last_node = ''
 
     def draw_graph(self):
         """
@@ -80,35 +83,47 @@ class Graph():
         # show graph
         plt.show()
 
+    def text_ask_name(self):
+        """
+        """
+        if self.current_node == self.last_node:
+            return "Sorry, I couldn't get your name. What's your name again?"
+
+        options = ["Hi! What's your name?",
+                   "Hello, could you remind me your name please?",
+                   "Hi! I can't recall your name. It was...?"]
+
+        return options[random.randint(0, len(options) - 1)]
+
     def text_offer_help(self):
         """
         """
-        return 'Hello! How can I help you?'
+        return "text_offer_help"
 
-    def text_ask_if_found(self):
+    def text_ask_problem(self):
         """
         """
-        return 'You should ask to the campus reception in case someone has found your card and has returned it there.'
+        return "text_ask_problem"
 
-    def text_ask_for_new_one(self):
+    def text_ask_check_in_reception(self):
         """
         """
-        return 'You should then ask for a new one, you can ask for a meeting in order to renew the student card following this link: http://fluvia.upf.edu/citescarnet/login.php. For more information you can always visit: www.upf.edu/carnetupf'
+        return "text_ask_check_in_reception"
 
-    def text_you_are_welcome(self):
+    def text_check_in_reception(self):
         """
         """
-        return "No problem. That is why I'm here :)"
+        return "text_check_in_reception"
 
     def text_goodbye(self):
         """
         """
-        return 'Goodbye!'
+        return "text_goodbye"
 
-    def text_error(self):
+    def text_get_new_card(self):
         """
         """
-        return "Sorry, I don't understand. Do you have any problem with your student card?"
+        return "text_get_new_card"
 
     def create_graph(self):
         """
@@ -117,42 +132,62 @@ class Graph():
         graph = nx.DiGraph()
 
         # add nodes
+        graph.add_node('ask_name', text=self.text_ask_name)
         graph.add_node('offer_help', text=self.text_offer_help)
-        graph.add_node('ask_if_found', text=self.text_ask_if_found)
-        graph.add_node('ask_for_new_one', text=self.text_ask_for_new_one)
-        graph.add_node('you_are_welcome', text=self.text_you_are_welcome)
+        graph.add_node('ask_problem', text=self.text_ask_problem)
+        graph.add_node('ask_check_in_reception', text=self.text_ask_check_in_reception)
+        graph.add_node('check_in_reception', text=self.text_check_in_reception)
         graph.add_node('goodbye', text=self.text_goodbye)
-        graph.add_node('error', text=self.text_error)
+        graph.add_node('get_new_card', text=self.text_get_new_card)
 
         # add edges
-        # todo: dos listas, una con posibilidades y otra con cosas que deben aparecer para ejecutar esa transicion?
-        graph.add_edge('offer_help', 'ask_if_found', text=["can't find", 'lost'])
-        graph.add_edge('offer_help', 'ask_for_new_one', text=["broken", 'not working', "doesn't work"])
-        graph.add_edge('offer_help', 'error', text=[])
+        graph.add_edge('ask_name', 'offer_help', text=[])
+        graph.add_edge('ask_name', 'ask_name', text=[])
 
-        graph.add_edge('ask_if_found', 'you_are_welcome', text=["thank you", 'thanks'])
-        graph.add_edge('ask_if_found', 'goodbye', text=['bye', 'goodbye'])
-        graph.add_edge('ask_if_found', 'ask_for_new_one', text=["already", 'done', 'asked'])
-        graph.add_edge('ask_if_found', 'error', text=[])
+        graph.add_edge('offer_help', 'ask_problem', text=["can't find", 'lost'])
+        graph.add_edge('offer_help', 'ask_check_in_reception', text=["can't find", 'lost'])
+        graph.add_edge('offer_help', 'get_new_card', text=["can't find", 'lost'])
+        graph.add_edge('offer_help', 'offer_help', text=["can't find", 'lost'])
 
-        graph.add_edge('ask_for_new_one', 'you_are_welcome', text=["thank you", 'thanks'])
-        graph.add_edge('ask_for_new_one', 'goodbye', text=['bye', 'goodbye'])
-        graph.add_edge('ask_for_new_one', 'error', text=[])
+        graph.add_edge('ask_problem', 'ask_check_in_reception', text=["can't find", 'lost'])
+        graph.add_edge('ask_problem', 'get_new_card', text=["can't find", 'lost'])
+        graph.add_edge('ask_problem', 'ask_problem', text=["can't find", 'lost'])
 
-        graph.add_edge('error', 'ask_if_found', text=["can't find", 'lost'])
-        graph.add_edge('error', 'ask_for_new_one', text=["broken", 'not working', "doesn't work"])
-        graph.add_edge('error', 'you_are_welcome', text=["thank you", 'thanks'])
-        graph.add_edge('error', 'goodbye', text=['bye', 'goodbye'])
-        graph.add_edge('error', 'error', text=[])
+        graph.add_edge('ask_check_in_reception', 'check_in_reception', text=["can't find", 'lost'])
+        graph.add_edge('ask_check_in_reception', 'get_new_card', text=["can't find", 'lost'])
+        graph.add_edge('ask_check_in_reception', 'ask_check_in_reception', text=["can't find", 'lost'])
+
+        graph.add_edge('check_in_reception', 'goodbye', text=["can't find", 'lost'])
+        graph.add_edge('check_in_reception', 'get_new_card', text=["can't find", 'lost'])
+        graph.add_edge('check_in_reception', 'check_in_reception', text=["can't find", 'lost'])
 
         self.graph = graph
 
-    def parse_answer(self, node, answer):
+    def parse_user_name(self, answer):
+        """
+        """
+        dummy_words = ['my', 'is', 'name', "i'm", 'am', 'i', 'hello', 'hi', ',', '.']
+        array_answer = [a for a in answer.lower().split(' ') if a not in dummy_words]
+        if len(array_answer) > 0 and len(array_answer) < 4:
+            self.user_name = ' '.join([a.capitalize() for a in array_answer])
+            print 'self.user_name: ', self.user_name
+        return
+
+    def parse_answer(self, answer):
         """ return number of the node to which we should transition, given that we
         are in 'node' and the user typed 'answer'
         """
+        
+        if self.current_node == 'ask_name':
+            self.parse_user_name(answer)
+            # return to ask_name
+            if self.user_name is None:
+                return (self.current_node, self.current_node)
+            else:
+                return ('ask_name', 'offer_help')
+
         # get edges connecting 'node' with other nodes
-        edges = [a for a in self.graph.edges() if a[0] == node]
+        edges = [a for a in self.graph.edges() if a[0] == self.current_node]
         
         # get first edge that contains any of the words on 'answer'
         for edge in edges:
@@ -161,18 +196,20 @@ class Graph():
                 return edge[1]
 
         # if no edge found, transition to 7
-        return 'error'
+        return (self.current_node, self.current_node)
 
     def create_dialog(self):
         """ starting on node 'offer_help', prints the machine answers according with
         the user questions, until no more transitions are possible
         """
         answer = raw_input("You enter Bea's virtual office. Welcome!\n>> ")  # 'hello' expected
-        node = 'offer_help'
-        while self.graph.successors(node):
-            answer = raw_input(self.graph.node[node]['text']() + '\n>> ')
-            edge = self.parse_answer(node, answer)
-            node = edge
+        while self.graph.successors(self.current_node):
+            print 'current_node: ', self.current_node
+            print 'last_node: ', self.last_node
+            answer = raw_input(self.graph.node[self.current_node]['text']() + '\n>> ')
+            edge = self.parse_answer(answer)
+            self.last_node = self.current_node
+            self.current_node = edge[1]
         print self.graph.node['goodbye']['text']
         return
 
