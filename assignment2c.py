4d62144bb063cb3ef2adaa1400de4cfb4ee48674
Laura Pérez Mayos <lpmayos@gmail.com>
2015-01-28 13:15:25 +0100

    complete state set and basic functions; still some improvement of the phrases needed

--- assignment2c.py
+++ assignment2c.py
@@ -25,11 +25,7 @@ References:
 
 Todos:
     - i si no té username U...?
-    - dates:
-        if not self.proposed_date:
-            self.proposed_date = datetime.datetime.now() + datetime.timedelta(days=2)
-        else:
-            self.proposed_date = self.proposed_date + datetime.timedelta(days=1)
+    - i si vol demanar hora més tard, etc? millorar funció ask_availability_function
 
 """
 
@@ -45,7 +41,7 @@ class Graph():
     """
 
     def __init__(self):
-        """
+        """ create the necessary class variables and compute the initial phrases
         """
         # state graph
         self.graph = None
@@ -54,7 +50,8 @@ class Graph():
         self.user_name = ""
         self.user_id = ""
         self.user_availability = None
-        self.proposed_date = datetime.datetime.now() + datetime.timedelta(days=2)
+        self.proposed_date = None
+        self.proposed_date_str = ""
 
         # state transition information
         self.current_node = 'ask_name'
@@ -105,8 +102,8 @@ class Graph():
         plt.show()
 
     def choose_phrase(self, node):
-        """ given a node name 'node', returns a valid random valid phrase from
-        the available phrases
+        """ given a node name 'node', returns a valid random phrase from the
+        available phrases
         """
         if self.last_node in self.states_phrases[node]['last_node']:
             # last_node was a special case for the node where we are
@@ -117,16 +114,16 @@ class Graph():
         else:
             # choose betwen regular options
             options = self.states_phrases[node]['options']
-        
+
         return options[random.randint(0, len(options) - 1)]
 
     def compute_phrases(self):
+        """ stores in self.state_phrases the valid phrases for every state.
+        For each state, stores a dictionary with the regular options in
+        'options', the phrase to show in case we couldn't find a transition and
+        are repeating the state in 'recurrent', and special phrases to show in
+        case we came from concrete states in 'last_node'
         """
-        """
-        hours = ['15:00', '16:00', '17:00', '17:30', '18:00']
-        if self.user_availability == 'morning':
-            hours = ['09:00', '10:00', '11:00', '11:30', '12:00']
-
         self.states_phrases = {'ask_name': {'options': ["Hi! What's your name?",
                                                         "Hello, could you remind me your name please?",
                                                         "Hi! I can't recall your name. It was...?"],
@@ -171,18 +168,90 @@ class Graph():
                                                            "Oh, it's the Punt d'Informació de l'Estudiantat. I can arrange a meeting for you if you want."],
                                                'recurrent': ["Sorry, I couldn't get what you just said. How can I help you?"],
                                                'last_node': {}},
-                               'ask_username': {'options': ["Ok. Can you give me your username? The one that starts with U...",
-                                                            "Perfect. I need you username, the one starting with U..."],
-                                                'recurrent': ["Sorry, I couldn't get what you just said. How can I help you?"],
-                                                'last_node': {'ask_availability': ["Sorry, can you type again your username? Something went wrong..."]}},
+                               'ask_userid': {'options': ["Ok. Can you give me your username? The one that starts with U...",
+                                                          "Perfect. I need you username, the one starting with U..."],
+                                              'recurrent': ["Sorry, I couldn't get what you just said. How can I help you?"],
+                                              'last_node': {'ask_availability': ["Sorry, can you type again your username? Something went wrong..."]}},
                                'ask_availability': {'options': ["Ok " + self.user_id + ". Do you prefer the meeting to be in the morning or in the afternoon?",
                                                                 self.user_id + ", got it. Would you like the meeting to be in the morning or in the afternoon?"],
                                                     'recurrent': ["Sorry, I couldn't get what you just said. How can I help you?"],
                                                     'last_node': {}},
-                               'check_date': {'options': ["The next possible appointment could be " + self.proposed_date.strftime("%B %d") + " at " + hours[random.randint(0, len(hours) - 1)],
-                                                          "If it is alright, I'll arrange you a meeting on " + self.proposed_date.strftime("%B %d") + " at " + hours[random.randint(0, len(hours) - 1)]],
+                               'check_date': {'options': ["The next possible appointment could be " + self.proposed_date_str,
+                                                          "If it is alright, I'll arrange you a meeting on " + self.proposed_date_str],
                                               'recurrent': ["Sorry, I couldn't get what you just said. How can I help you?"],
                                               'last_node': {}}}
+
+    def parse_user_name(self, answer):
+        """ finds the user name on the answer, by deleting all the dummy words;
+        if after deleting them more than 3 words remain, no name has been found.
+        """
+        dummy_words = ['my', 'is', 'name', "i'm", 'am', 'i', 'hello', 'hi', ',', '.']
+        array_answer = [a for a in answer if a not in dummy_words]
+        if len(array_answer) > 0 and len(array_answer) < 4:
+            self.user_name = ' '.join([a.capitalize() for a in array_answer])
+            self.compute_phrases()  # recompute phrases because some of them contain self.user_name
+        return
+
+    def parse_user_id(self, answer):
+        """ finds the user id on the answer by looking for a 7 letters word
+        starting with 'u' followed by 6 digits, and stores it on self.user_id
+        """
+        for word in answer:
+            if word[0] == 'u' and len(word) == 7 and word[1:7].isdigit():
+                self.user_id = word
+                self.compute_phrases()  # recompute phrases because some of them contain self.user_id
+        return
+
+    def ask_name_function(self, answer):
+        """ finds the user name on the answer and stores it on self.user_name.
+        returns a transition to offer_help if we found it, a recurrent
+        transition otherwise
+        """
+        self.parse_user_name(answer)
+        # return to ask_name
+        if self.user_name == "":
+            return (self.current_node, self.current_node)
+        else:
+            return ('ask_name', 'offer_help')
+
+    def ask_userid_function(self, answer):
+        """ finds the user id on the answer and stores it on self.user_id.
+        returns a transition to ask_availability if we found it, a recurrent
+        transition otherwise
+        """
+        self.parse_user_id(answer)
+        if self.user_id == "":
+            return (self.current_node, self.current_node)
+        else:
+            return ('ask_userid', 'ask_availability')
+
+    def ask_availability_function(self, answer):
+        """ identifies if the user would prefer a meeting in the morning or in
+        the afternoon, and saves a proposed date on self.proposed_date.
+        If it is not the first proposed date, it proposes a date on the next day
+        """
+        if 'morning' in answer:
+            self.user_availability = 'morning'
+        elif 'afternoon' in answer:
+            self.user_availability = 'afternoon'
+
+        # if user had not decided before and doesn't say anything now, 'morning' by default
+        if not self.user_availability:
+            self.user_availability = 'morning'
+
+        hours = ['15:00', '16:00', '17:00', '17:30', '18:00']
+        if self.user_availability == 'morning':
+            hours = ['09:00', '10:00', '11:00', '11:30', '12:00']
+
+        if not self.proposed_date:
+            self.proposed_date = datetime.datetime.now() + datetime.timedelta(days=2)
+        else:
+            self.proposed_date = self.proposed_date + datetime.timedelta(days=1)
+
+        self.proposed_date_str = self.proposed_date.strftime("%B %d") + " at " + hours[random.randint(0, len(hours) - 1)]
+
+        self.compute_phrases()  # recompute phrases because some of them contain self.proposed_date_str
+        return
 
     def create_graph(self):
         """ creates the graph with all possible nodes and transitions; saves it
@@ -191,12 +260,16 @@ class Graph():
         # create networkx graph
         graph = nx.DiGraph()
 
-        # add nodes
-        graph.add_nodes_from(['ask_name', 'offer_help', 'ask_problem',
+        # add regular nodes
+        graph.add_nodes_from(['offer_help', 'ask_problem',
                               'ask_check_in_reception', 'check_in_reception',
                               'goodbye', 'get_new_card', 'check_permissions',
-                              'meeting_pie', 'ask_username', 'ask_availability',
-                              'check_date'])
+                              'meeting_pie', 'check_date'])
+
+        # add nodes that need special ways of choose_next_node
+        graph.add_node('ask_name', function=self.ask_name_function)
+        graph.add_node('ask_userid', function=self.ask_userid_function)
+        graph.add_node('ask_availability', function=self.ask_availability_function)
 
         # add edges
         graph.add_edge('ask_name', 'offer_help', required_words=[])
@@ -231,18 +304,18 @@ class Graph():
         graph.add_edge('check_permissions', 'check_permissions', required_words=[])
 
         graph.add_edge('meeting_pie', 'goodbye', required_words=['no', 'not', 'myself'])
-        graph.add_edge('meeting_pie', 'ask_username', required_words=['yes', 'please', 'sure'])
+        graph.add_edge('meeting_pie', 'ask_userid', required_words=['yes', 'please', 'sure'])
         graph.add_edge('meeting_pie', 'explain_pie', required_words=['pie', 'sorry', 'explain'])
         graph.add_edge('meeting_pie', 'meeting_pie', required_words=[])
 
-        graph.add_edge('explain_pie', 'ask_username', required_words=['yes', 'please', 'sure'])
+        graph.add_edge('explain_pie', 'ask_userid', required_words=['yes', 'please', 'sure'])
         graph.add_edge('explain_pie', 'goodbye', required_words=['no', 'not', 'myself'])
         graph.add_edge('explain_pie', 'explain_pie', required_words=[])
 
-        graph.add_edge('ask_username', 'ask_availability', required_words=[])
-        graph.add_edge('ask_username', 'ask_username', required_words=[])
+        graph.add_edge('ask_userid', 'ask_availability', required_words=[])
+        graph.add_edge('ask_userid', 'ask_userid', required_words=[])
 
-        graph.add_edge('ask_availability', 'ask_username', required_words=['no', 'mistake', 'error', 'wrong', 'sorry'])
+        graph.add_edge('ask_availability', 'ask_userid', required_words=['no', 'mistake', 'error', 'wrong', 'sorry'])
         graph.add_edge('ask_availability', 'check_date', required_words=['afternoon', 'morning', 'care', 'soon', 'sooner', 'same'])
         graph.add_edge('ask_availability', 'ask_availability', required_words=[])
 
@@ -251,17 +324,6 @@ class Graph():
         graph.add_edge('check_date', 'check_date', required_words=[])
 
         self.graph = graph
-
-    def parse_user_name(self, answer):
-        """ finds the user name on teh answer, by deleting all the dummy words;
-        if after deleting them more than 3 words remain, no name has been found.
-        """
-        dummy_words = ['my', 'is', 'name', "i'm", 'am', 'i', 'hello', 'hi', ',', '.']
-        array_answer = [a for a in answer.lower().split(' ') if a not in dummy_words]
-        if len(array_answer) > 0 and len(array_answer) < 4:
-            self.user_name = ' '.join([a.capitalize() for a in array_answer])
-            self.compute_phrases()  # recompute phrases because some of them contain self.user_name
-        return
 
     def parse_answer(self, answer):
         """ cleans the answer to be able to split it into words, by separating
@@ -274,21 +336,21 @@ class Graph():
         """ returns the edge to which we should transition, given that we are in
         self.current_node and the user typed 'answer'
         """
-        # special cases
-        if self.current_node == 'ask_name':
-            self.parse_user_name(answer)
-            # return to ask_name
-            if self.user_name == "":
-                return (self.current_node, self.current_node)
-            else:
-                return ('ask_name', 'offer_help')
-
         # parse user answer
         answer = self.parse_answer(answer)
 
+        # special cases
+
+        if 'function' in self.graph.node[self.current_node]:
+            edge = self.graph.node[self.current_node]['function'](answer)
+            if edge:
+                return edge
+
+        # normal cases
+
         # get edges connecting 'node' with other nodes
         edges = [a for a in self.graph.edges() if a[0] == self.current_node]
-        
+
         # get edge that contains more keyword coincidences with 'answer'
         max_required_words = 0
         best_edge = None
@@ -304,7 +366,7 @@ class Graph():
                 need_recheck = False
             elif num_required_words == max_required_words:
                 need_recheck = True
-        
+
         if need_recheck:
             return (self.current_node, self.current_node)
         elif best_edge:
