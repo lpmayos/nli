5b8486dd39dd3a558a2a187ea587174e2a9d7255
Laura Pérez Mayos <lpmayos@gmail.com>
2015-02-16 01:08:04 +0100

    refactor inicial

--- assignment3.py
+++ assignment3.py
@@ -1,211 +1,224 @@
-def print_chart():
-    """
-    """
-    print '\n\n\n\n'
-    for i, col in enumerate(chart):
-        print '\n----------------------------------------------------------------------------------------------------------------'
-        print 'chart[', i, ']'
-        print '----------------------------------------------------------------------------------------------------------------'
-        aux = 0
-        for j, row in enumerate(chart[i]):
-
-            rule = row['rule']
-            rule = rule.split(' ')
-            try:
-                rule[row['dot_pos']] = '. ' + rule[row['dot_pos']]
-            except:
-                rule.append('.')
-
-            if 'pointer_to_parent' in row:
-                print aux, '   ', row['key_rule'], ' --> ', ' '.join(rule), '               [', row['begin'], ', ', row['end'], ']               ', row['who_added_it'], ', ', row['pointer_to_parent'], '\n'
-            else:
-                print aux, '   ', row['key_rule'], ' --> ', ' '.join(rule), '               [', row['begin'], ', ', row['end'], ']               ', row['who_added_it'], '\n'
-            aux+=1
+import sys
 
 
-def next_cat(state):
-    """
-    """
-    return state['rule'].split()[state['dot_pos']]
-
-
-def next_cats(state):
-    """
-    """
-    return state['rule'].split()[state['dot_pos']::]
-
-
-def state_in_chart_pos(new_state, chart_pos):
-    """
-    """
-    for state in chart[chart_pos]:
-        if state['key_rule'] == new_state['key_rule'] and state['rule'] == new_state['rule'] and state['begin'] == new_state['begin'] and state['end'] == new_state['end'] and state['dot_pos'] == new_state['dot_pos'] and state['who_added_it'] == new_state['who_added_it'] and state['complete'] == new_state['complete']:
-            return True
-    return False
-
-
-def enqueue(state, chart_pos):
-    """
-    """
-    try:
-        if not state_in_chart_pos(state, chart_pos):
-            chart[chart_pos].append(state)
-    except:
-        return
-    return
-
-
-def earley_parser(words):
-    """
-    """
-    chart[0].append({'key_rule': 'gamma',
-                     'rule': 's',
-                     'begin': 0,
-                     'end': 0,
-                     'dot_pos': 0,
-                     'who_added_it': 'dummy start state',
-                     'complete': False})
-
-    for i in range(len(chart)):
-        if i < len(words):
-            word = words[i]
-
-        for k, state in enumerate(chart[i]):
-            if not state['complete'] and next_cat(state) not in final_words:
-                predictor(state)
-            elif not state['complete'] and next_cat(state) in final_words:
-                scanner(state, word)
-            else:
-                completer(state, i, k)
-
-    return chart
-
-
-def predictor(state):
-    """
-    """
-    next_categories = next_cats(state)
-    for next_category in next_categories:
-        if next_category in grammar:
-            for expansion in grammar[next_category]:
-                new_state = {'key_rule': next_category,
-                             'rule': expansion,
-                             'begin': state['end'],
-                             'end': state['end'],
-                             'dot_pos': 0,
-                             'who_added_it': 'predictor',
-                             'complete': False}
-                enqueue(new_state, state['end'])
-    return
-
-
-def scanner(state, word):
-    """
-    """
-    rule = state['rule'].split()[state['dot_pos']]
-    if rule in grammar and word in grammar[rule]:
-        new_state = {'key_rule': rule,
-                     'rule': word,
-                     'begin': state['begin'],
-                     'end': state['end'] + 1,
-                     'dot_pos': state['dot_pos'] + 1,
-                     'who_added_it': 'scanner',
-                     'complete': state['dot_pos'] + 1 >= len(word.split())} 
-        enqueue(new_state, state['end'] + 1)
-    return
-
-
-def completer(current_state, column, row):
+class EarleyParser():
     """
     """
 
-    for chart_pos in chart[int(current_state['begin']):int(current_state['end']) + 1]:  # VIK MOD
-        for state in chart_pos:
-            if not state['complete'] and state['rule'].split()[state['dot_pos']] == current_state['key_rule']:
-                if 'pointer_to_parent' in state:
-                    col1 = state['pointer_to_parent'][0][0]
-                    row1 = state['pointer_to_parent'][0][1]
-                    new_state = {'key_rule': state['key_rule'],
-                                 'rule': state['rule'],
-                                 'begin': state['begin'],
-                                 'end': current_state['end'],
-                                 'dot_pos': state['dot_pos'] + 1,
-                                 'who_added_it': 'completer',
-                                 'complete': state['dot_pos'] + 1 == len(state['rule'].split()),
-                                 'pointer_to_parent': [[column, row]]}
-                    new_state['pointer_to_parent'].append([col1,row1])
+    def __init__(self, words, final_words, grammar):
+        """
+        """
+        self.words = words
+        self.final_words = final_words
+        self.grammar = grammar
+        self.chart = [[] for x in range(len(words) + 1)]
 
+    def print_chart(self):
+        """
+        """
+        print '\n\n\n\n'
+
+        for i, col in enumerate(self.chart):
+            print '\n----------------------------------------------------------------------------------------------------------------'
+            print 'chart[', i, ']'
+            print '----------------------------------------------------------------------------------------------------------------'
+            aux = 0
+
+            for j, row in enumerate(self.chart[i]):
+
+                rule = row['rule']
+                rule = rule.split(' ')
+                try:
+                    rule[row['dot_pos']] = '. ' + rule[row['dot_pos']]
+                except:
+                    rule.append('.')
+
+                if 'pointer_to_parent' in row:
+                    print aux, '   ', row['key_rule'], ' --> ', ' '.join(rule), '               [', row['begin'], ', ', row['end'], ']               ', row['who_added_it'], ', ', row['pointer_to_parent'], '\n'
                 else:
-                    new_state = {'key_rule': state['key_rule'],
-                                 'rule': state['rule'],
-                                 'begin': state['begin'],
-                                 'end': current_state['end'],
-                                 'dot_pos': state['dot_pos'] + 1,
-                                 'who_added_it': 'completer',
-                                 'complete': state['dot_pos'] + 1 == len(state['rule'].split()),
-                                 'pointer_to_parent': [[column, row]]}
-                enqueue(new_state, current_state['end'])
-            if state == current_state:
-                break
-    return
+                    print aux, '   ', row['key_rule'], ' --> ', ' '.join(rule), '               [', row['begin'], ', ', row['end'], ']               ', row['who_added_it'], '\n'
+                aux += 1
 
+    def next_cat(self, state):
+        """
+        """
+        return state['rule'].split()[state['dot_pos']]
 
-def extract_parsing_trees():
-    """
-    """
-    for i, state in enumerate(chart[len(words)]):
-        if state['key_rule'] == 'gamma' and state['end'] == len(words):
-            final_state = state
+    def next_cats(self, state):
+        """
+        """
+        return state['rule'].split()[state['dot_pos']::]
 
-    final_set = []
+    def state_in_chart_pos(self, new_state, chart_pos):
+        """
+        """
+        for state in self.chart[chart_pos]:
+            if state['key_rule'] == new_state['key_rule'] and state['rule'] == new_state['rule'] and state['begin'] == new_state['begin'] and state['end'] == new_state['end'] and state['dot_pos'] == new_state['dot_pos'] and state['who_added_it'] == new_state['who_added_it'] and state['complete'] == new_state['complete']:
+                return True
+        return False
 
-    row = 99
-    col = 99
+    def enqueue(self, state, chart_pos):
+        """
+        """
+        try:
+            if not self.state_in_chart_pos(state, chart_pos):
+                self.chart[chart_pos].append(state)
+        except:
+            return
+        return
 
-    while True:
-        if final_state["key_rule"] + " --> " + final_state["rule"] not in final_set: 
-            final_set.append(final_state["key_rule"] + " --> " + final_state["rule"])
-        if row == 0:
-            for state in chart[col - 1]:
-                if 'pointer_to_parent' in state:
-                    final_state = state
-                    if final_state["key_rule"] + " --> " + final_state["rule"] not in final_set: 
-                        final_set.append(final_state["key_rule"] + " --> " + final_state["rule"])
+    def earley_parser(self):
+        """
+        """
+        self.chart[0].append({'key_rule': 'gamma',
+                              'rule': 's',
+                              'begin': 0,
+                              'end': 0,
+                              'dot_pos': 0,
+                              'who_added_it': 'dummy start state',
+                              'complete': False})
+
+        for i in range(len(self.chart)):
+            if i < len(self.words):
+                word = self.words[i]
+
+            for k, state in enumerate(self.chart[i]):
+                if not state['complete'] and self.next_cat(state) not in self.final_words:
+                    self.predictor(state)
+                elif not state['complete'] and self.next_cat(state) in self.final_words:
+                    self.scanner(state, word)
+                else:
+                    self.completer(state, i, k)
+
+        return self.chart
+
+    def predictor(self, state):
+        """
+        """
+        next_categories = self.next_cats(state)
+        for next_category in next_categories:
+            if next_category in self.grammar:
+                for expansion in self.grammar[next_category]:
+                    new_state = {'key_rule': next_category,
+                                 'rule': expansion,
+                                 'begin': state['end'],
+                                 'end': state['end'],
+                                 'dot_pos': 0,
+                                 'who_added_it': 'predictor',
+                                 'complete': False}
+                    self.enqueue(new_state, state['end'])
+        return
+
+    def scanner(self, state, word):
+        """
+        """
+        rule = state['rule'].split()[state['dot_pos']]
+        if rule in self.grammar and word in self.grammar[rule]:
+            new_state = {'key_rule': rule,
+                         'rule': word,
+                         'begin': state['begin'],
+                         'end': state['end'] + 1,
+                         'dot_pos': state['dot_pos'] + 1,
+                         'who_added_it': 'scanner',
+                         'complete': state['dot_pos'] + 1 >= len(word.split())} 
+            self.enqueue(new_state, state['end'] + 1)
+        return
+
+    def completer(self, current_state, column, row):
+        """
+        """
+
+        for chart_pos in self.chart[int(current_state['begin']):int(current_state['end']) + 1]:
+            for state in chart_pos:
+                if not state['complete'] and state['rule'].split()[state['dot_pos']] == current_state['key_rule']:
+                    if 'pointer_to_parent' in state:
+                        col1 = state['pointer_to_parent'][0][0]
+                        row1 = state['pointer_to_parent'][0][1]
+                        new_state = {'key_rule': state['key_rule'],
+                                     'rule': state['rule'],
+                                     'begin': state['begin'],
+                                     'end': current_state['end'],
+                                     'dot_pos': state['dot_pos'] + 1,
+                                     'who_added_it': 'completer',
+                                     'complete': state['dot_pos'] + 1 == len(state['rule'].split()),
+                                     'pointer_to_parent': [[column, row]]}
+                        new_state['pointer_to_parent'].append([col1, row1])
+
+                    else:
+                        new_state = {'key_rule': state['key_rule'],
+                                     'rule': state['rule'],
+                                     'begin': state['begin'],
+                                     'end': current_state['end'],
+                                     'dot_pos': state['dot_pos'] + 1,
+                                     'who_added_it': 'completer',
+                                     'complete': state['dot_pos'] + 1 == len(state['rule'].split()),
+                                     'pointer_to_parent': [[column, row]]}
+                    self.enqueue(new_state, current_state['end'])
+                if state == current_state:
                     break
-        if row == 0 and col == 1:
-            break
-        else:
-            row = final_state['pointer_to_parent'][0][1]
-            col = final_state['pointer_to_parent'][0][0]
-            if len(final_state['pointer_to_parent']) == 2:
-                row1 = final_state['pointer_to_parent'][1][1]
-                col1 = final_state['pointer_to_parent'][1][0]
-                if chart[col1][row1]["key_rule"] + ' --> ' + chart[col1][row1]["rule"] not in final_set:
-                    final_set.append(chart[col1][row1]["key_rule"] + ' --> ' + chart[col1][row1]["rule"])
-            final_state = chart[col][row]
+        return
 
-    print final_set
+    def extract_parsing_trees(self):
+        """
+        """
+        for i, state in enumerate(self.chart[len(self.words)]):
+            if state['key_rule'] == 'gamma' and state['end'] == len(self.words):
+                final_state = state
 
-    return
+        final_set = []
+
+        row = 99
+        col = 99
+
+        while True:
+            if final_state["key_rule"] + " --> " + final_state["rule"] not in final_set:
+                final_set.append(final_state["key_rule"] + " --> " + final_state["rule"])
+            if row == 0:
+                for state in self.chart[col - 1]:
+                    if 'pointer_to_parent' in state:
+                        final_state = state
+                        if final_state["key_rule"] + " --> " + final_state["rule"] not in final_set:
+                            final_set.append(final_state["key_rule"] + " --> " + final_state["rule"])
+                        break
+            if row == 0 and col == 1:
+                break
+            else:
+                row = final_state['pointer_to_parent'][0][1]
+                col = final_state['pointer_to_parent'][0][0]
+                if len(final_state['pointer_to_parent']) == 2:
+                    row1 = final_state['pointer_to_parent'][1][1]
+                    col1 = final_state['pointer_to_parent'][1][0]
+                    if self.chart[col1][row1]["key_rule"] + ' --> ' + self.chart[col1][row1]["rule"] not in final_set:
+                        final_set.append(self.chart[col1][row1]["key_rule"] + ' --> ' + self.chart[col1][row1]["rule"])
+                final_state = self.chart[col][row]
+
+        print final_set
+
+        return
 
 
-phrase = "you need new permissions"
-# phrase = "I can arrange a meeting for you"
-# phrase = "the next possible appointment could be tomorrow"
+def initial_data(num_phrase):
+    """
+    """
 
-final_words = ['pronoun', 'det', 'adj', 'n', 'md', 'v', 'prep']
+    phrases = ["you need new permissions",
+               "I can arrange a meeting for you",
+               "the next possible appointment could be tomorrow"]
 
-grammar = {'s': ['np vp'],
-           'np': ['pronoun', 'adj n', 'np pp', 'det n', 'n', 'det adj adj n'],
-           'pp': ['prep pronoun'],
-           'vp': ['v np', 'md vp'],
-           'pronoun': ['you', 'I'],
-           'det': ['a', 'the'],
-           'prep': ['for'],
-           'v': ['need', 'be', 'arrange'],
-           'adj': ['new', 'next', 'possible'],
-           'md': ['can', 'could'],
-           'n': ['permissions', 'meeting', 'appointment', 'tomorrow']}
+    final_words = ['pronoun', 'det', 'adj', 'n', 'md', 'v', 'prep']
+
+    grammar = {'s': ['np vp'],
+               'np': ['pronoun', 'adj n', 'np pp', 'det n', 'n', 'det adj adj n'],
+               'pp': ['prep pronoun'],
+               'vp': ['v np', 'md vp'],
+               'pronoun': ['you', 'I'],
+               'det': ['a', 'the'],
+               'prep': ['for'],
+               'v': ['need', 'be', 'arrange'],
+               'adj': ['new', 'next', 'possible'],
+               'md': ['can', 'could'],
+               'n': ['permissions', 'meeting', 'appointment', 'tomorrow']}
+    return phrases[num_phrase].split(), final_words, grammar
 
 # testing data from http://www.inf.ed.ac.uk/teaching/courses/inf2a/slides/2007_inf2a_L18_slides.pdf
 # phrase = "fish swim in the soup"
@@ -237,8 +250,14 @@ grammar = {'s': ['np vp'],
 #            'prep': ['from', 'to', 'on'],
 #            'proper_noun': ['houston', 'twa']}
 
-chart = [[] for x in range(len(phrase.split()) + 1)]
-words = phrase.split()
-earley_parser(words)
-print_chart()
-extract_parsing_trees()
+
+def main():
+    words, final_words, grammar = initial_data(1)
+    earley_parser = EarleyParser(words, final_words, grammar)
+    earley_parser.earley_parser()
+    earley_parser.print_chart()
+    earley_parser.extract_parsing_trees()
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
